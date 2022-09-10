dd1556b966c7abc25ae6343c580d848cf1316e7a
JosephAkim <Udokimjoseph@gmail.com>
2022-09-10 12:46:19 +0100

    Register Validation

--- app/src/main/java/com/kim/shopingapp/fragment/loginRegister/RegisterFragment.kt
+++ app/src/main/java/com/kim/shopingapp/fragment/loginRegister/RegisterFragment.kt
@@ -1,16 +1,25 @@
 package com.kim.shopingapp.fragment.loginRegister
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.kim.shopingapp.R
+import com.kim.shopingapp.data.User
 import com.kim.shopingapp.databinding.FragmentRegisterBinding
+import com.kim.shopingapp.util.RegisterValidation
+import com.kim.shopingapp.util.Resource
 import com.kim.shopingapp.viewmodel.RegisterViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.withContext
 
+private val TAG = "RegisterFragment"
 @AndroidEntryPoint
 class RegisterFragment: Fragment() {
 
@@ -28,5 +37,56 @@ class RegisterFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            buttonRegisterRegister.setOnClickListener {
+                val user = User(
+                    edFirstNameRegister.text.toString().trim(),
+                    edLastNameRegister.text.toString().trim(),
+                    edEmailRegister.text.toString().trim()
+                )
+                val password = edPasswordRegister.text.toString()
+                viewModel.createAccountWithEmailAndPassword(user, password)
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.register.collect() {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.buttonRegisterRegister.startAnimation()
+                    }
+                    is Resource.Success -> {
+                        Log.d("test", it.data.toString())
+                        binding.buttonRegisterRegister.revertAnimation()
+                    }
+                    is Resource.Error -> {
+                        Log.e(TAG, it.message.toString())
+                        binding.buttonRegisterRegister.revertAnimation()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.validation.collect{ validation ->
+                if (validation.email is RegisterValidation.Failed){
+                    withContext(Dispatchers.Main){
+                        binding.edEmailRegister.apply {
+                            requestFocus()
+                            error = validation.email.message
+                        }
+                    }
+                }
+                if (validation.password is RegisterValidation.Failed){
+                    withContext(Dispatchers.Main){
+                        binding.edPasswordRegister.apply {
+                            requestFocus()
+                            error = validation.password.message
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/kim/shopingapp/viewmodel/RegisterViewModel.kt
+++ app/src/main/java/com/kim/shopingapp/viewmodel/RegisterViewModel.kt
@@ -4,10 +4,13 @@ import androidx.lifecycle.ViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.kim.shopingapp.data.User
-import com.kim.shopingapp.util.Resource
+import com.kim.shopingapp.util.*
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @HiltViewModel
@@ -15,10 +18,17 @@ class RegisterViewModel @Inject constructor(
     private val firebaseAuth: FirebaseAuth
     ): ViewModel() {
 
-    private val _register = MutableStateFlow<Resource<FirebaseUser>>(Resource.Loading())
+    private val _register = MutableStateFlow<Resource<FirebaseUser>>(Resource.Unspecified())
     val register: Flow<Resource<FirebaseUser>> = _register
 
+    private val _validation = Channel<RegisterFieldState> ()
+    val validation = _validation.receiveAsFlow()
+
         fun createAccountWithEmailAndPassword(user:User, password:String){
+            if (checkValidation(user, password)) {
+                runBlocking {
+                _register.emit(Resource.Loading())
+            }
             firebaseAuth.createUserWithEmailAndPassword(user.email, password)
                 .addOnSuccessListener {
                     it.user?.let {
@@ -27,6 +37,23 @@ class RegisterViewModel @Inject constructor(
                 }.addOnFailureListener {
                     _register.value = Resource.Error(it.message.toString())
                 }
+            }else{
+                val registerFieldState = RegisterFieldState(
+                    validateEmail(user.email), validatePassword(password)
+                )
+                runBlocking {
+                    _validation.send(registerFieldState)
+                }
+            }
         }
+
+    private fun checkValidation(user: User, password: String): Boolean {
+        val emailValidation = validateEmail(user.email)
+        val passwordValidation = validatePassword(password)
+        val shouldRegister = emailValidation is RegisterValidation.Success &&
+                passwordValidation is RegisterValidation.Success
+
+        return shouldRegister
+    }
 
 }
